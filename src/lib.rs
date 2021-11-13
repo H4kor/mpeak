375ded9f3754c272fcc6e2b0db69118b51ee1ed1
Niko Abeler <niko@rerere.org>
2021-11-13 19:29:12 +0100

    renaming + list of all headers

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@
 
 use std::io::prelude::*;
 use std::fs::File;
+use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub enum MPeakError {
@@ -16,7 +17,7 @@ pub enum MPeakError {
 
 #[derive(Debug, PartialEq)]
 #[repr(u8)]
-pub enum MpegVersion{
+pub enum Mp3Version{
     V25 = 0,
     Reserved = 1,
     V2 = 2,
@@ -25,7 +26,7 @@ pub enum MpegVersion{
 
 #[derive(Debug, PartialEq)]
 #[repr(u8)]
-pub enum MpegLayer {
+pub enum Mp3Layer {
     Reserved = 0,
     Layer3 = 1,
     Layer2 = 2,
@@ -33,14 +34,14 @@ pub enum MpegLayer {
 }
 
 #[derive(Debug, PartialEq)]
-pub enum MpegProtection {
+pub enum Mp3Protection {
     ProtectedByCrc,
     NotProtected,
 }
 
 #[derive(Debug, PartialEq)]
 #[repr(u8)]
-pub enum MpegChannelMode {
+pub enum Mp3ChannelMode {
     Stereo = 0,
     JointStereo = 1,
     DualChannel = 2,
@@ -50,15 +51,14 @@ pub enum MpegChannelMode {
 
 #[derive(Debug, PartialEq)]
 #[repr(u8)]
-pub enum MpegEmphasis {
+pub enum Mp3Emphasis {
     None = 1,
     _50_15Ms = 2,
     Reserved = 3,
     CcitJ17 = 4,
 }
 
-#[derive(Debug, PartialEq)]
-pub struct MpegFrameHeader {
+pub struct Mp3FrameHeader {
     //            AAAAAAAA AAABBCCD EEEEFFGH IIJJKLMM
     // sync:      11111111 11100000 00000000 00000000
     // version:   00000000 00011000 00000000 00000000
@@ -89,45 +89,45 @@ const MPEG2: [u16; 4] = [ 22050, 24000, 16000, 0 ];
 const MPEG2_5: [u16; 4] = [ 11025, 12000, 8000, 0 ];
 const MPEGR: [u16; 4] = [ 0, 0, 0, 0 ];
 
-impl MpegFrameHeader {
-    pub fn new(data: u32) -> MpegFrameHeader {
-        MpegFrameHeader{
+impl Mp3FrameHeader {
+    pub fn new(data: u32) -> Mp3FrameHeader {
+        Mp3FrameHeader{
             data: data
         }
     }
 
-    pub fn version(&self) -> MpegVersion {
+    pub fn version(&self) -> Mp3Version {
         let bits: (bool, bool) = (
             self.data >> 20 & 1 == 1,
             self.data >> 19 & 1 == 1
         );
         match bits {
-            (false, false) => MpegVersion::V25,
-            (false, true) => MpegVersion::Reserved,
-            (true, false) => MpegVersion::V2,
-            (true, true) => MpegVersion::V1,
+            (false, false) => Mp3Version::V25,
+            (false, true) => Mp3Version::Reserved,
+            (true, false) => Mp3Version::V2,
+            (true, true) => Mp3Version::V1,
         }
     }
 
 
-    pub fn layer(&self) -> MpegLayer {
+    pub fn layer(&self) -> Mp3Layer {
         let bits: (bool, bool) = (
             self.data >> 18 & 1 == 1,
             self.data >> 17 & 1 == 1
         );
         match bits {
-            (false, false) => MpegLayer::Reserved,
-            (false, true) => MpegLayer::Layer3,
-            (true, false) => MpegLayer::Layer2,
-            (true, true) => MpegLayer::Layer1,
+            (false, false) => Mp3Layer::Reserved,
+            (false, true) => Mp3Layer::Layer3,
+            (true, false) => Mp3Layer::Layer2,
+            (true, true) => Mp3Layer::Layer1,
         }
     }
 
-    pub fn protected(&self) -> MpegProtection {
+    pub fn protected(&self) -> Mp3Protection {
         if self.data >> 16 & 1 == 1 {
-            MpegProtection::NotProtected
+            Mp3Protection::NotProtected
         } else {
-            MpegProtection::ProtectedByCrc
+            Mp3Protection::ProtectedByCrc
         }
     }
 
@@ -147,16 +147,16 @@ impl MpegFrameHeader {
         self.data >> 8 & 1 == 1
     }
 
-    pub fn channel_mode(&self) -> MpegChannelMode {
+    pub fn channel_mode(&self) -> Mp3ChannelMode {
         let bits: (bool, bool) = (
             self.data >> 7 & 1 == 1,
             self.data >> 6 & 1 == 1
         );
         match bits {
-            (false, false) => MpegChannelMode::Stereo,
-            (false, true) => MpegChannelMode::JointStereo,
-            (true, false) => MpegChannelMode::DualChannel,
-            (true, true) => MpegChannelMode::SingleChannel,
+            (false, false) => Mp3ChannelMode::Stereo,
+            (false, true) => Mp3ChannelMode::JointStereo,
+            (true, false) => Mp3ChannelMode::DualChannel,
+            (true, true) => Mp3ChannelMode::SingleChannel,
         }
     }
 
@@ -173,41 +173,41 @@ impl MpegFrameHeader {
         self.data >> 2 & 1 == 1
     }
 
-    pub fn emphasis(&self) -> MpegEmphasis {
+    pub fn emphasis(&self) -> Mp3Emphasis {
         let bits: (bool, bool) = (
             self.data >> 1 & 1 == 1,
             self.data >> 0 & 1 == 1
         );
         match bits {
-            (false, false) => MpegEmphasis::None,
-            (false, true) => MpegEmphasis::_50_15Ms,
-            (true, false) => MpegEmphasis::Reserved,
-            (true, true) => MpegEmphasis::CcitJ17,
+            (false, false) => Mp3Emphasis::None,
+            (false, true) => Mp3Emphasis::_50_15Ms,
+            (true, false) => Mp3Emphasis::Reserved,
+            (true, true) => Mp3Emphasis::CcitJ17,
         }
     }
 
     pub fn frame_length(&self) -> usize {
         let bitrate_list = match self.version() {
-            MpegVersion::V1 => match self.layer() {
-                MpegLayer::Layer1 => V1_L1,
-                MpegLayer::Layer2 => V1_L2,
-                MpegLayer::Layer3 => V1_L3,
-                MpegLayer::Reserved => VR_LR
+            Mp3Version::V1 => match self.layer() {
+                Mp3Layer::Layer1 => V1_L1,
+                Mp3Layer::Layer2 => V1_L2,
+                Mp3Layer::Layer3 => V1_L3,
+                Mp3Layer::Reserved => VR_LR
             },
-            MpegVersion::V25 | MpegVersion::V2 =>  match self.layer() {
-                MpegLayer::Layer1 => V2_L1,
-                MpegLayer::Layer2 | MpegLayer::Layer3 => V2_L2_3,
-                MpegLayer::Reserved => VR_LR
+            Mp3Version::V25 | Mp3Version::V2 =>  match self.layer() {
+                Mp3Layer::Layer1 => V2_L1,
+                Mp3Layer::Layer2 | Mp3Layer::Layer3 => V2_L2_3,
+                Mp3Layer::Reserved => VR_LR
             },
-            MpegVersion::Reserved => VR_LR
+            Mp3Version::Reserved => VR_LR
         };
         let bitrate = bitrate_list[self.bitrate_index() as usize];
 
         let sample_rate_list = match self.version() {
-            MpegVersion::V1 => MPEG1,
-            MpegVersion::V2 => MPEG2,
-            MpegVersion::V25 => MPEG2_5,
-            MpegVersion::Reserved => MPEGR,
+            Mp3Version::V1 => MPEG1,
+            Mp3Version::V2 => MPEG2,
+            Mp3Version::V25 => MPEG2_5,
+            Mp3Version::Reserved => MPEGR,
         };
         let sample_rate = sample_rate_list[self.sampling_rate_index() as usize];
         let frame_len: u32 = 144 * (bitrate as u32) * 1000 / (sample_rate as u32);
@@ -216,6 +216,16 @@ impl MpegFrameHeader {
         } else {
             frame_len as usize
         }
+    }
+}
+
+
+impl fmt::Debug for Mp3FrameHeader {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Mp3FrameHeader")
+            .field("version", &self.version())
+            .field("layer", &self.layer())
+            .finish()
     }
 }
 
@@ -275,10 +285,23 @@ pub fn get_id3_data(data: &Vec<u8>) -> Vec<u8> {
 }
 
 
-pub fn get_first_mp3_frame_header(data: &Vec<u8>) -> MpegFrameHeader {
+
+pub fn get_first_mp3_frame_header(data: &Vec<u8>) -> Mp3FrameHeader {
     let offset = get_id3_offset(data) as usize;
     let header_data = u32::from_be_bytes(data[offset..offset+4].try_into().unwrap());
-    MpegFrameHeader::new(header_data)
+    Mp3FrameHeader::new(header_data)
+}
+
+pub fn all_headers(data: &Vec<u8>) -> Vec<Mp3FrameHeader> {
+    let mut offset = get_id3_offset(data) as usize;
+    let mut headers = Vec::<Mp3FrameHeader>::new();
+    while offset < data.len() {
+        let header_data = u32::from_be_bytes(data[offset..offset+4].try_into().unwrap());
+        let curr = Mp3FrameHeader::new(header_data);
+        offset += curr.frame_length();
+        headers.push(curr);
+    }
+    headers
 }
 
 #[cfg(test)]
@@ -313,139 +336,139 @@ mod tests {
 
     #[test]
     fn test_frame_header_version() {
-        let header = MpegFrameHeader::new(0b00000000000_00_0000000000000000000);
-        assert_eq!(header.version(), MpegVersion::V25);
-        let header = MpegFrameHeader::new(0b00000000000_01_0000000000000000000);
-        assert_eq!(header.version(), MpegVersion::Reserved);
-        let header = MpegFrameHeader::new(0b00000000000_10_0000000000000000000);
-        assert_eq!(header.version(), MpegVersion::V2);
-        let header = MpegFrameHeader::new(0b00000000000_11_0000000000000000000);
-        assert_eq!(header.version(), MpegVersion::V1);
+        let header = Mp3FrameHeader::new(0b00000000000_00_0000000000000000000);
+        assert_eq!(header.version(), Mp3Version::V25);
+        let header = Mp3FrameHeader::new(0b00000000000_01_0000000000000000000);
+        assert_eq!(header.version(), Mp3Version::Reserved);
+        let header = Mp3FrameHeader::new(0b00000000000_10_0000000000000000000);
+        assert_eq!(header.version(), Mp3Version::V2);
+        let header = Mp3FrameHeader::new(0b00000000000_11_0000000000000000000);
+        assert_eq!(header.version(), Mp3Version::V1);
     }
 
 
     #[test]
     fn test_frame_header_layer() {
-        let header = MpegFrameHeader::new(0b0000000000000_00_00000000000000000);
-        assert_eq!(header.layer(), MpegLayer::Reserved);
-        let header = MpegFrameHeader::new(0b0000000000000_01_00000000000000000);
-        assert_eq!(header.layer(), MpegLayer::Layer3);
-        let header = MpegFrameHeader::new(0b0000000000000_10_00000000000000000);
-        assert_eq!(header.layer(), MpegLayer::Layer2);
-        let header = MpegFrameHeader::new(0b0000000000000_11_00000000000000000);
-        assert_eq!(header.layer(), MpegLayer::Layer1);
+        let header = Mp3FrameHeader::new(0b0000000000000_00_00000000000000000);
+        assert_eq!(header.layer(), Mp3Layer::Reserved);
+        let header = Mp3FrameHeader::new(0b0000000000000_01_00000000000000000);
+        assert_eq!(header.layer(), Mp3Layer::Layer3);
+        let header = Mp3FrameHeader::new(0b0000000000000_10_00000000000000000);
+        assert_eq!(header.layer(), Mp3Layer::Layer2);
+        let header = Mp3FrameHeader::new(0b0000000000000_11_00000000000000000);
+        assert_eq!(header.layer(), Mp3Layer::Layer1);
     }
 
     #[test]
     fn test_frame_header_protected() {
-        let header = MpegFrameHeader::new(0b_00000000_00000001_00000000_00000000);
-        assert_eq!(header.protected(), MpegProtection::NotProtected);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000000);
-        assert_eq!(header.protected(), MpegProtection::ProtectedByCrc);
+        let header = Mp3FrameHeader::new(0b_00000000_00000001_00000000_00000000);
+        assert_eq!(header.protected(), Mp3Protection::NotProtected);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000000);
+        assert_eq!(header.protected(), Mp3Protection::ProtectedByCrc);
     }
 
     #[test]
     fn test_frame_header_bitrate_index() {
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000000);
         assert_eq!(header.bitrate_index(), 0);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_11110000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_11110000_00000000);
         assert_eq!(header.bitrate_index(), 15);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_11000000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_11000000_00000000);
         assert_eq!(header.bitrate_index(), 12);
     }
 
     #[test]
     fn test_frame_header_sampling_rate_index() {
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000000);
         assert_eq!(header.sampling_rate_index(), 0);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000100_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000100_00000000);
         assert_eq!(header.sampling_rate_index(), 1);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00001000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00001000_00000000);
         assert_eq!(header.sampling_rate_index(), 2);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00001100_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00001100_00000000);
         assert_eq!(header.sampling_rate_index(), 3);
     }
 
     #[test]
     fn test_frame_header_padding_bit() {
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000010_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000010_00000000);
         assert_eq!(header.padding_bit(), true);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000000);
         assert_eq!(header.padding_bit(), false);
     }
 
     #[test]
     fn test_frame_header_private_bit() {
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000001_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000001_00000000);
         assert_eq!(header.private_bit(), true);
     }
 
     #[test]
     fn test_frame_header_channel_mode() {
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000000);
-        assert_eq!(header.channel_mode(), MpegChannelMode::Stereo);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_01000000);
-        assert_eq!(header.channel_mode(), MpegChannelMode::JointStereo);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_10000000);
-        assert_eq!(header.channel_mode(), MpegChannelMode::DualChannel);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_11000000);
-        assert_eq!(header.channel_mode(), MpegChannelMode::SingleChannel);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000000);
+        assert_eq!(header.channel_mode(), Mp3ChannelMode::Stereo);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_01000000);
+        assert_eq!(header.channel_mode(), Mp3ChannelMode::JointStereo);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_10000000);
+        assert_eq!(header.channel_mode(), Mp3ChannelMode::DualChannel);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_11000000);
+        assert_eq!(header.channel_mode(), Mp3ChannelMode::SingleChannel);
     }
 
     #[test]
     fn test_frame_header_mode_extension() {
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000000);
         assert_eq!(header.mode_extension(), 0);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00010000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00010000);
         assert_eq!(header.mode_extension(), 1);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00100000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00100000);
         assert_eq!(header.mode_extension(), 2);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00110000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00110000);
         assert_eq!(header.mode_extension(), 3);
     }
 
     #[test]
     fn test_frame_header_copyright() {
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00001000);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00001000);
         assert_eq!(header.copyright(), true);
     }
 
     #[test]
     fn test_frame_header_original() {
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000100);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000100);
         assert_eq!(header.original(), true);
     }
     
     #[test]
     fn test_frame_header_emphasis() {
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000000);
-        assert_eq!(header.emphasis(), MpegEmphasis::None);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000001);
-        assert_eq!(header.emphasis(), MpegEmphasis::_50_15Ms);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000010);
-        assert_eq!(header.emphasis(), MpegEmphasis::Reserved);
-        let header = MpegFrameHeader::new(0b_00000000_00000000_00000000_00000011);
-        assert_eq!(header.emphasis(), MpegEmphasis::CcitJ17);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000000);
+        assert_eq!(header.emphasis(), Mp3Emphasis::None);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000001);
+        assert_eq!(header.emphasis(), Mp3Emphasis::_50_15Ms);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000010);
+        assert_eq!(header.emphasis(), Mp3Emphasis::Reserved);
+        let header = Mp3FrameHeader::new(0b_00000000_00000000_00000000_00000011);
+        assert_eq!(header.emphasis(), Mp3Emphasis::CcitJ17);
     }
 
     #[test]
     fn test_frame_length() {
         // V1 Layer 1 birate 4 samplerate 0
-        let header = MpegFrameHeader::new(0b_00000000_00011110_01000000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00011110_01000000_00000000);
         assert_eq!(header.frame_length(), 417);
         // V1 Layer 2 birate 13 samplerate 1
-        let header = MpegFrameHeader::new(0b_00000000_00011100_11010100_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00011100_11010100_00000000);
         assert_eq!(header.frame_length(), 960);
 
         // V2 Layer 1 birate 2 samplerate 2
-        let header = MpegFrameHeader::new(0b_00000000_00010110_00101000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00010110_00101000_00000000);
         assert_eq!(header.frame_length(), 432);
         // V2 layer 3 birate 8 samplerate 2
-        let header = MpegFrameHeader::new(0b_00000000_00010010_10001000_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00010010_10001000_00000000);
         assert_eq!(header.frame_length(), 576);
 
         // V2 layer 3 birate 8 samplerate 2 + padding
-        let header = MpegFrameHeader::new(0b_00000000_00010010_10001010_00000000);
+        let header = Mp3FrameHeader::new(0b_00000000_00010010_10001010_00000000);
         assert_eq!(header.frame_length(), 577);
     }
 
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,16 @@ fn main() {
     let file_result = mpeak::load_file(file_path);
     match file_result {
         Ok(file_data) => {
-            println!("is mp3:     {:?}", mpeak::is_mp3_file(&file_data));
-            println!("has id3:    {:?}", mpeak::has_id3(&file_data));
-            println!("id3 offset: {:?}", mpeak::get_id3_offset(&file_data));
-            println!("id3 data:   {:?}", String::from_utf8_lossy(&mpeak::get_id3_data(&file_data)));
-            let header = mpeak::get_first_mp3_frame_header(&file_data);
-            println!("header         {:?}", header);
-            println!("header frame_length {:?}", header.frame_length());
+            println!("is mp3:             {:?}", mpeak::is_mp3_file(&file_data));
+            println!("has id3:            {:?}", mpeak::has_id3(&file_data));
+            println!("id3 offset:         {:?}", mpeak::get_id3_offset(&file_data));
+            println!("id3 data:           {:?}", String::from_utf8_lossy(&mpeak::get_id3_data(&file_data)));
+            let headers = mpeak::all_headers(&file_data);
+            println!("frames              {:?}", headers.len());
+            println!("header              {:?}", headers[0]);
+            println!("header frame_length {:?}", headers[0].frame_length());
+
+
         },
         Err(e) => println!("{:?}", e)
     };
